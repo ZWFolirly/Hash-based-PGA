5dea644b8d04db02876f211d337116d00dd48f75
ZWFolirly <32008078+ZWFolirly@users.noreply.github.com>
2019-06-13 10:01:11 +0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/ParallelHGA.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Administrator/Desktop/MEGA/MEGAsync/C_Project/ParallelHGA/Graph.cpp" "C:/Users/Administrator/Desktop/MEGA/MEGAsync/C_Project/ParallelHGA/cmake-build-debug/CMakeFiles/ParallelHGA.dir/Graph.cpp.obj"
+  "C:/Users/Administrator/Desktop/MEGA/MEGAsync/C_Project/ParallelHGA/PGenetic.cpp" "C:/Users/Administrator/Desktop/MEGA/MEGAsync/C_Project/ParallelHGA/cmake-build-debug/CMakeFiles/ParallelHGA.dir/PGenetic.cpp.obj"
+  "C:/Users/Administrator/Desktop/MEGA/MEGAsync/C_Project/ParallelHGA/PHashGenetic.cpp" "C:/Users/Administrator/Desktop/MEGA/MEGAsync/C_Project/ParallelHGA/cmake-build-debug/CMakeFiles/ParallelHGA.dir/PHashGenetic.cpp.obj"
+  "C:/Users/Administrator/Desktop/MEGA/MEGAsync/C_Project/ParallelHGA/main.cpp" "C:/Users/Administrator/Desktop/MEGA/MEGAsync/C_Project/ParallelHGA/cmake-build-debug/CMakeFiles/ParallelHGA.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ParallelHGA.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParallelHGA.dir/main.cpp.obj"
+  "CMakeFiles/ParallelHGA.dir/PGenetic.cpp.obj"
+  "CMakeFiles/ParallelHGA.dir/PHashGenetic.cpp.obj"
+  "CMakeFiles/ParallelHGA.dir/Graph.cpp.obj"
+  "ParallelHGA.pdb"
+  "ParallelHGA.exe"
+  "ParallelHGA.exe.manifest"
+  "libParallelHGA.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ParallelHGA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
